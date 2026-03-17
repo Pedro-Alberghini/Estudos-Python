0546d8f6301adf5f0fd6fed58a2f7ee31b2c5143
Pedro Alberghini <pedro.dataengineer@hotmail.com>
2026-03-17 00:18:36 -0300

    Operadores
    Criando todos os operadores simples em Python

---
+++ Scripts/operadores.py
@@ -0,0 +1,12 @@
+n1 = input("Digite um valor:")
+n2 = input("Digite outro valor:")
+
+soma = int(n1) + int(n2)
+sub = int(n1) - int(n2)
+mult = int(n1) * int(n2)
+div = int(n1) / int(n2)
+
+print("A soma é:", soma)
+print("A subtração é:", sub)
+print("A multiplicação é:", mult)
+print("A divisão é:", div)
